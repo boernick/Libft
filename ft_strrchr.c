2306bbbccb68f0b2c1c743a3e75474267b869327
Nick Boer <nboer@c3a5c1.42berlin.de>
2024-05-22 12:04:25 +0200

    final

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 14:59:12 by nboer             #+#    #+#             */
-/*   Updated: 2024/05/20 20:53:48 by nboer            ###   ########.fr       */
+/*   Updated: 2024/05/22 12:03:54 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
+	int		len;
 	char	*p;
 
-	p = NULL;
-	while (*str != '\0')
+	len = ft_strlen(str);
+	p = (char *)str + len;
+	if ((char)c == '\0')
+		return (p);
+	while (len-- >= 0)
 	{
-		if (*str == (char)c)
-			p = (char *) str;
-		str++;
+		if (*p == (char)c)
+			return (p);
+		p--;
 	}
-	if (c == '\0')
-	{
-		return ((char *)str);
-	}
-	return ((char *)p);
+	return (NULL);
 }
